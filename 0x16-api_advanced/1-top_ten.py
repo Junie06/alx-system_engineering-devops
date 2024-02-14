02a9320f618a625801107dc2efdc7348eee6c1a7
Isoken Ibizugbe <isokenjune@gmail.com>
2024-02-14 00:17:48 +0000

    api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''queries the Reddit API and prints the titles of the first 10 hot posts'''
+
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+
+    data = requests.get(url, headers=header, allow_redirects=False)
+
+    if data.status_code == 200:
+        data = data.json().get("data").get("children")
+        for element in data:
+            print(element.get("data").get("title"))
+    else:
+        print(None)
