557d884bb65c437a1e72641edf40def25a50fe4f
Isoken Ibizugbe <isokenjune@gmail.com>
2024-02-22 11:45:10 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,9 @@ file { '/etc/default/nginx':
   ensure  => file,
   content => "ULIMIT='-n 2048'\n",
 }
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
